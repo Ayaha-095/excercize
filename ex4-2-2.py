2ae85587bb9bed88768d0c7509f521e073c688bb
Ayaha-095 <ayaha.miura@nds-osk.co.jp>
2025-05-16 17:12:11 +0900

    ex4-2-1.py,ex4-2-2.py,ex4-2-3.pyをコミット

--- ex4-2-2.py
+++ ex4-2-2.py
@@ -1,8 +1,4 @@
 import random
-# l: list = []
-#
-# for i in range(100):
-#     l.append(random.randint(0, 99))
 
 l: list = [random.randint(0, 99) for i in range(100)]
-print(l)
+print(set(l))
---
+++ ex4-2-3.py
@@ -0,0 +1,5 @@
+import random
+
+# l: list = [random.randint(0, 99) for i in range(100)]
+s0 = {random.randint(0, 99) for i in range(100)}
+print(s0)
